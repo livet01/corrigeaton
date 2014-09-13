41a802718604a7c35823380602f111561670241c
realsoc <hugo.djemaa@gmail.com>
2014-09-13 23:10:55 +0200

    ADEService findclassroomname

--- src/Corrigeaton/Bundle/ScheduleBundle/Service/ADEService.php
+++ src/Corrigeaton/Bundle/ScheduleBundle/Service/ADEService.php
@@ -4,9 +4,14 @@ namespace Corrigeaton\Bundle\ScheduleBundle\Service;
 class ADEService
 {
 
-    public function findClassroomName($url)
+    public function findClassroomName($classNum)
     {
-
-
+        $url = "https://www.etud.insa-toulouse.fr/planning/index.php?gid="+$classNum+"&wid=0&ics=1";
+        $week =  file_get_contents($url);
+        $tout = "";
+        $name = "";
+        $res = [1 => $tout, 2=>$name];
+        preg_match("CALNAME:([^ ]*)", $week, $res);
+        return $name;
     }
 }
