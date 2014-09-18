1370b73e90ca21f487fa7d090c825abf512dfcc3
realsoc <hugo.djemaa@gmail.com>
2014-09-18 15:02:26 +0200

    ReportListener

--- src/Corrigeaton/Bundle/ReportBundle/Listener/ReportListener.php
+++ src/Corrigeaton/Bundle/ReportBundle/Listener/ReportListener.php
@@ -13,15 +13,29 @@ use Corrigeaton\Bundle\ReportBundle\Entity\Report;
 use Corrigeaton\Bundle\ReportBundle\Event\ReportEvent;
 
 class ReportListener {
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
     public function onReportEvent(ReportEvent $reportEvent)
     {
-        //Créer un report
         $report = new Report();
         $classReportEvent = $reportEvent->getClass();
         $reflectedClass = new \ReflectionClass($classReportEvent);
         $report->setLog($reportEvent->getLog());
-        $report->setData($reflectedClass->getProperties());
-        $report->setType($classReportEvent);
+        $reflectedProperties = $reflectedClass->getProperties();
+        $data = array();
+        foreach ($reflectedProperties as $reflectedProperty)
+        {
+            $data[$reflectedProperty->getName()] = $reflectedProperty->getValue();
+        }
+        $report->setData($data);
+        $report->setType($classReportEvent->get_class());
+        $this->$em->persist($report);
+        $this->em->flush();
+
     }
+
 
 } 
