0f56d566877cf32fa92a14b50106345ae30f81f7
realsoc <hugo.djemaa@gmail.com>
2014-09-18 13:50:04 +0200

    Report entity

---
+++ src/Corrigeaton/Bundle/ReportBundle/Entity/Report.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Corrigeaton\Bundle\ReportBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Report
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Report
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="log", type="string", length=255)
+     */
+    private $log;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="data", type="array")
+     */
+    private $data;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set log
+     *
+     * @param string $log
+     * @return Report
+     */
+    public function setLog($log)
+    {
+        $this->log = $log;
+
+        return $this;
+    }
+
+    /**
+     * Get log
+     *
+     * @return string 
+     */
+    public function getLog()
+    {
+        return $this->log;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Report
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Report
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set data
+     *
+     * @param array $data
+     * @return Report
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Get data
+     *
+     * @return array 
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+}
